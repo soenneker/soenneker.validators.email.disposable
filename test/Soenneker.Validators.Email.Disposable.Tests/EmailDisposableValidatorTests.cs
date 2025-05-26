076adddb3b80d233d8b32bdde70cd531f80f5b11
Jake Soenneker <jake@soeneker.com>
2025-05-26 23:38:45 +0000

    FluentAssertions -> AwesomeAssertions namespace

--- test/Soenneker.Validators.Email.Disposable.Tests/EmailDisposableValidatorTests.cs
+++ test/Soenneker.Validators.Email.Disposable.Tests/EmailDisposableValidatorTests.cs
@@ -1,5 +1,5 @@
 using System.Threading.Tasks;
-using FluentAssertions;
+using AwesomeAssertions;
 using Soenneker.Validators.Email.Disposable.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
